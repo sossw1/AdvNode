401b85121b43e6e3943d21ee1ddb4bfdaf5695ab
sossw1 <sossw1@gmail.com>
2022-08-09 22:19:35 -0400

    exec prototype for cache service

---
+++ services/cache.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {
+
+  const result = await exec.apply(this, arguments);
+
+  return result;
+};
