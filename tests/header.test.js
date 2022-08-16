14f754877b60769ba1c0a69bf4b3203a87b95e64
sossw1 <sossw1@gmail.com>
2022-08-15 20:58:28 -0400

    session string and signature

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,7 +1,12 @@
 const puppeteer = require('puppeteer');
+const { Buffer } = require('safe-buffer');
+const Keygrip = require('keygrip');
+const keys = require('../config/keys');
 
 let browser;
 let page;
+
+const keygrip = new Keygrip([keys.cookieKey]);
 
 beforeEach(async () => {
   browser = await puppeteer.launch({ headless: false });
@@ -22,4 +27,13 @@ test('Clicking login starts OAuth flow', async () => {
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  const id = '62f12d7f5923663064e2213a';
+  const sessionObject = { passport: { user: id } };
+  const sessionString = Buffer.from(
+    JSON.stringify(sessionObject)
+  ).toString('base64');
+  const sig = keygrip.sign('session=' + sessionString);
 });
