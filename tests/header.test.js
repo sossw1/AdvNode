a7c134fb38a3da11942d7b5275d34a16c38ebfb2
sossw1 <sossw1@gmail.com>
2022-08-17 15:21:50 -0400

    getContentsOf function replaces complex syntax

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,7 +12,7 @@ afterEach(async () => {
 });
 
 test('Header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo');
   expect(text).toEqual('Blogster');
 });
 
@@ -26,6 +26,6 @@ test('Clicking login starts OAuth flow', async () => {
 
 test('When signed in, shows logout button', async () => {
   await page.login();
-  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
   expect(text).toEqual('Logout');
 });
